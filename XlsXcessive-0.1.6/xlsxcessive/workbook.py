858072c34941e90b96e04d3d4542b7f1ce531ac4
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2017-12-25 18:05:40 -0800

    new module
    added new excel module

---
+++ XlsXcessive-0.1.6/xlsxcessive/workbook.py
@@ -0,0 +1,25 @@
+from xlsxcessive import markup
+from xlsxcessive.style import Stylesheet, Format
+from xlsxcessive.worksheet import Worksheet
+
+
+class Workbook(object):
+    def __init__(self):
+        self.sheets = []
+        self.stylesheet = Stylesheet(self)
+        self.date1904 = False # do not change this value when you already inserted dates!
+
+    def new_sheet(self, name):
+        sid = len(self.sheets) + 1
+        sheet = Worksheet(self, name, sid, "rId%d" % sid)
+        self.sheets.append(sheet)
+        return sheet
+
+    def new_format(self):
+        return Format(self)
+
+    def __str__(self):
+        sheet_references = "".join(s.ref for s in self.sheets)
+        return markup.workbook % {'date1904':'true' if self.date1904 else 'false', 
+                                  'sheets':sheet_references}
+
