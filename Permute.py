1846fa598b4241ba51b669d4613759f8efdd7e1d
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2017-12-25 16:59:52 -0800

    First edits
    Added Permute Python file, and a text file output

---
+++ Permute.py
@@ -0,0 +1,12 @@
+import itertools
+import sys
+
+value = list(itertools.permutations(["English 2B",2,3,4,5,6,7,8]))
+# sys.stdout = open("Test.txt", "w")
+# print (value)
+# print ("Test")
+
+
+filename  = open("Test.txt",'w')
+sys.stdout = filename
+print (value)
