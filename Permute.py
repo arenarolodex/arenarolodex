c6c38a83a09dfb9a7db3e4ba3eaa4d52fb6ed644
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2017-12-25 17:38:52 -0800

    testing
    fooling around

--- Permute.py
+++ Permute.py
@@ -1,5 +1,6 @@
 import itertools
 import sys
+import xlsxwriter
 
 value = list(itertools.permutations(["English 2B",2,3,4,5,6,7,8]))
 # sys.stdout = open("Test.txt", "w")
@@ -7,6 +8,14 @@ value = list(itertools.permutations(["English 2B",2,3,4,5,6,7,8]))
 # print ("Test")
 
 
-filename  = open("Test.xlsx",'w')
-sys.stdout = filename
-print (value)
+# filename  = open("Test.xlsx","w")
+# sys.stdout = filename
+# print (value)
+
+
+workbook = xlsxwriter.Workbook('hello.xlsx')
+worksheet = workbook.add_worksheet()
+
+worksheet.write('A1-H40320', value)
+
+workbook.close()
