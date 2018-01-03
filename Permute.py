b52ebc6bc526671d60ca0b836d46a91d43f20c9a
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2018-01-03 00:24:11 -0800

    more changes
    added more code and more files related to html-flask (so close yet so far)

--- Permute.py
+++ Permute.py
@@ -3,31 +3,41 @@ import sys
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+#app.debug = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('my-form.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/template', methods=['POST'])
 def index_post():
-    block1 = request.form['block1']
-    block2 = request.form['block2']
-    block3 = request.form['block3']
-    block4 = request.form['block4']
-    block5 = request.form['block5']
-    block6 = request.form['block6']
-    block7 = request.form['block7']
-    block8 = request.form['block8']
+    with app.test_request_context():
+        block1 = request.form['block1']
+        block2 = request.form['block2']
+        block3 = request.form['block3']
+        block4 = request.form['block4']
+        block5 = request.form['block5']
+        block6 = request.form['block6']
+        block7 = request.form['block7']
+        block8 = request.form['block8']
 
     mylist = [block1, block2, block3, block4, block5, block6, block7, block8]
-    return mylist
+    #global valued 
+    valued = list(itertools.permutations(mylist))
+    return valued
 
-value = list(itertools.permutations(index_post()))
+
 # sys.stdout = open("Test.txt", "w")
 # print (value)
 # print ("Test")
 
+if __name__ == "__main__":
+    app.run()
 
-filename  = open("Test.txt","w")
-sys.stdout = filename
-print (value)
+print (index_post)
+
+
+
+# filename  = open("Test.txt","w")
+# sys.stdout = filename
+
