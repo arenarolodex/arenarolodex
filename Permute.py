e88d09d2a6a3df1b281e6405a8a8eaf6d9bf099f
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2018-01-06 00:26:23 -0800

    minor python changes
    added framework for parsing the announcer

--- Permute.py
+++ Permute.py
@@ -14,6 +14,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/template', methods=['GET', 'POST'])
+def block_retriever():
+    app.logger.debug('This block of code was reached. congrats')
+    block1 = request.form['block1']
+    block2 = request.form['block2']
+    block3 = request.form['block3']
+    block4 = request.form['block4']
+    block5 = request.form['block5']
+    block6 = request.form['block6']
+    block7 = request.form['block7']
+    block8 = request.form['block8']
+
+    retrieved = block1 + ',' + block2 + ',' + block3 + ',' + block4 + ',' + block5 + ',' + block6 + ',' + block7 + ',' + block8
+    return retrieved
+
+
+
 @app.route('/template', methods=['GET', 'POST'])
 def index_post():
     app.logger.debug('This block of code was reached. congrats')
@@ -32,34 +50,23 @@ def index_post():
 
     if mylist[0] != "":
         mylist1.append(block1)
-
     if mylist[1] != "":
         mylist1.append(block2)
-
     if mylist[2] != "":
         mylist1.append(block3)
-
     if mylist[3] != "":
         mylist1.append(block4)
-
     if mylist[4] != "":
         mylist1.append(block5)
-
     if mylist[5] != "":
         mylist1.append(block6)
-
     if mylist[6] != "":
         mylist1.append(block7)
-
     if mylist[7] != "":
         mylist1.append(block8)
-
-
-    
     # global valued
     valued = list(itertools.permutations(mylist1))
     # ret = '</br>'.join(', '.join(elems) for elems in valued)
-
     # ret = '\n'.join(', '.join(elems) for elems in valued)
     # return ret
     # return jsonify(valued)
@@ -71,7 +78,6 @@ def index_post():
     with open ('filelanding.csv', 'w', newline='') as f_in:
         writing = csv.writer(f_in, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
         writing.writerows(valued)
-
         # json.dump(ret, f)
     return render_template('landing.html')
 
