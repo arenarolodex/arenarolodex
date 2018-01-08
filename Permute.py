f9016e14f283a927e7c31996cae45d20e4289a7f
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-01-08 10:16:30 -0800

    Ignored cache files, use for each loop in code

--- Permute.py
+++ Permute.py
@@ -49,25 +49,12 @@ def index_post():
     block8 = request.form['block8']
 
     mylist = [block1, block2, block3, block4, block5, block6, block7, block8]
-    
+
     mylist1 = []
 
-    if mylist[0] != "":
-        mylist1.append(block1)
-    if mylist[1] != "":
-        mylist1.append(block2)
-    if mylist[2] != "":
-        mylist1.append(block3)
-    if mylist[3] != "":
-        mylist1.append(block4)
-    if mylist[4] != "":
-        mylist1.append(block5)
-    if mylist[5] != "":
-        mylist1.append(block6)
-    if mylist[6] != "":
-        mylist1.append(block7)
-    if mylist[7] != "":
-        mylist1.append(block8)
+    for x in mylist:
+        if x != "":
+            mylist1.append(x)
 
     if all(v == "" for v in mylist):
         return render_template('landing.html')
@@ -87,7 +74,7 @@ def index_post():
 
     # for elems in valued
     #     part = []
-    #     for 
+    #     for
 
 
 
@@ -96,7 +83,7 @@ def index_post():
 def rowcleaner():
     with open ('filelanding.csv', 'r', newline='') as f_in, open('fileoutput.csv', 'w') as f_out:
         writer = csv.writer(f_out, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-        
+
         for row in csv.reader(f_in, delimiter=','):
             if row[0] != "a":
                 writer.writerow(row)
