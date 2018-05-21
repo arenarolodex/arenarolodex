6d78053f72aa07f9b0ab8acb61eee2bada5777e2
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-05-21 00:25:26 -0700

    Temporary cache fix - Disable cache for every request

--- arenarolodex/base.py
+++ arenarolodex/base.py
@@ -5,6 +5,17 @@ from arenarolodex import app
 def index():
     return render_template('index.html')
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0, no-cache, no-store, must-revalidate'
+    return r
+
 
 # @app.route('/index', methods=['GET', 'POST'])
 # def button1_retrieve():
