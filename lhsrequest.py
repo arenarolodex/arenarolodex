ebdb7dfc4c0275c06c70b6993241b0bd90751704
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-05-22 00:06:49 -0700

    New requirements?

--- lhsrequest.py
+++ lhsrequest.py
@@ -1,28 +1,15 @@
 import requests
 import csv
-import os
-import redis
-from rq import Worker, Queue, Connection
-
 from bs4 import BeautifulSoup
-
-listen = ['high', 'default', 'low']
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-conn = redis.from_url(redis_url)
-
-if __name__ == 'main':
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
 
 def update_options2():
     url="http://lowell-courseselection.org"
     headers = {'Cookie': 'AspxAutoDetectCookieSupport=1;.ASPXAUTH=D3644034D3DB38E1E89813DCED3B2A28C4046E5EDE836B42CABB9E42D8A26B8E5E2EDA001CDDB7D988A764C1BFD186B2FD87A03CF50E7672216A09DC54A658DCDE225CA953838E926E7B034A2601E51F4CEC7E27AB459DFAA20B00B0A7C342480743814538F19E26B7631BBC48BD3C864745FCB6A03E18DF14BEC5940D5D9791709094AA43D1FBADE44D5F3A07E68036'}
     r = requests.post(url, headers=headers)
-    
+
     soup = BeautifulSoup(r.text, 'html.parser')
     table = soup.find(id="MainContent_GridView1")
-    
+
     with open('options2.csv', 'w', newline='') as csvout:
         writer = csv.writer(csvout, delimiter=',',
                             quotechar='\"', quoting=csv.QUOTE_MINIMAL)
