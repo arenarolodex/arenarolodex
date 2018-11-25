0cf5db59ffe30afeebfeb7f6e062489889d706c7
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-11-24 23:07:19 -0800

    Extend Course component - Add dropdowns for preferred teacher, block, class, and subject - Update some documentation

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Lowell Arena Helper
 This is the source code for arenarolodex, a web app that creates combinations of
 schedules for Lowell High School students.
+
+## How to develop
+This fork uses Gatsby and Firebase.
+To start developing this project, you will need:
+- [node.js and npm](https://nodejs.org/)
+- [the Gatsby CLI](https://www.gatsbyjs.org/) (you can install this by running
+  `npm install -g gatsby-cli` in your terminal)
--- src/components/courses.js
+++ src/components/courses.js
@@ -2,43 +2,80 @@ import React from 'react'
 
 import styles from './courses.module.css'
 
+/**A container for all the courses from user input.*/
 export default class Courses extends React.Component{
   constructor() {
     super();
     this.state = {courses: []};
   }
-  componentDidMount() {
-    //mount
+  handleSumbit(e) {
+    e.preventDefault();
+    alert("Form was submitted.");
+    //TODO Handle submission of course selection here
   }
-  componentWillUnmount() {
-    //unmount
-  }
+
   render() {
-    const courses = this.state.courses.map((key)=>
-      <Course key={key} />
-    );
     return (
-      <div>
+      <form onSubmit={this.handleSumbit}>
         <div>
-          {courses}
+          {this.state.courses}
         </div>
         <button onClick={this.addcourse.bind(this)}>Add class</button>
-      </div>
+        <input type="submit" value="Find schedules" />
+      </form>
     );
   }
   addcourse(e){
     e.preventDefault();
     var state = this.state;
-    state.courses.push((new Date()).getTime());
+    state.courses.push(<Course key={(new Date()).getTime()} />);
     this.setState(state);
   }
 }
+
+/**An individual Course where the user inputs their class, preferred teacher
+and block, etc.*/
 class Course extends React.Component{
+  constructor(){
+    super();
+    this.state = {class: null, subject: null, prefTeach: null, prefBlock: null,
+      priorityTeach: 1, priorityBlock: 1};
+  }
   render(){
     return (
-      <div class={styles.course}>
-        Test
+      <div className={styles.course}>
+        <CourseSelect name="Class" />
+        <CourseSelect name="Teacher" />
+        <CourseSelect name="Subject" options='{"optionArray": ["Math","Science"]}' />
+        <CourseSelect name="Block" />
       </div>
+    );
+  }
+}
+
+class CourseSelect extends React.Component{
+  constructor() {
+    super();
+    this.state = {options:[]};
+  }
+  componentDidMount() {
+    if (this.props.options !== undefined) {
+      this.setState({options: JSON.parse(this.props.options).optionArray});
+      console.log(this.state.options);
+    }
+  }
+  render() {
+    return (
+      // <label onChange={this.props.onchange}>
+      //TODO write onChange handler for component, pass changes to parent Course
+      <label>
+        {this.props.name}
+        <select>
+          {this.state.options.map((option) =>
+            <option key={option} value={option}>{option}</option>
+          )}
+        </select>
+      </label>
     );
   }
 }
