e4490b21b42888f2cb958c347c6e0afc2d2172b0
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-11-29 09:52:47 -0800

    Update state of Courses component on selection change - Make handleChange() on Courses component level

--- src/components/courses.js
+++ src/components/courses.js
@@ -6,29 +6,42 @@ import styles from './courses.module.css'
 export default class Courses extends React.Component{
   constructor() {
     super();
-    this.state = {courses: []};
+    this.state = {courses: {}};
+    this.handleChange = this.handleChange.bind(this);
   }
   handleSumbit(e) {
     e.preventDefault();
-    alert("Form was submitted.");
+    var selection = Object.keys(this.state.courses).map((key) => (this.state.courses[key]));
+    alert("Form was submitted. "+JSON.stringify(selection));
     //TODO Handle submission of course selection here
   }
-
   render() {
     return (
-      <form onSubmit={this.handleSumbit}>
+      <form onSubmit={this.handleSumbit.bind(this)}>
         <div>
-          {this.state.courses}
+          {Object.keys(this.state.courses).map((key) =>
+            (<Course changeHandler={this.handleChange} id={key} key={key}
+              options={this.state.courses[key].options} />)
+          )}
         </div>
         <button onClick={this.addcourse.bind(this)}>Add class</button>
         <input type="submit" value="Find schedules" />
       </form>
     );
   }
+  handleChange(key, type, value) {
+    //TODO Handle <select> changes at the Courses component level
+    console.log("Course "+key+" changed its "+type+" to "+value);
+    var state = this.state;
+    state.courses[key][type] = value;
+    this.setState(state);
+  }
   addcourse(e){
     e.preventDefault();
     var state = this.state;
-    state.courses.push(<Course key={(new Date()).getTime()} />);
+    var key = (new Date()).getTime()
+    state.courses[key] = {Class: null, Subject: null, prefTeach: null, prefBlock: null,
+      priorityTeach: 1, priorityBlock: 1, options: {"Subject": ["","Math","Science"]}};
     this.setState(state);
   }
 }
@@ -36,57 +49,36 @@ export default class Courses extends React.Component{
 /**An individual Course where the user inputs their class, preferred teacher
 and block, etc.*/
 class Course extends React.Component{
-  constructor(){
+  constructor() {
     super();
-    this.state = {class: null, subject: null, prefTeach: null, prefBlock: null,
-      priorityTeach: 1, priorityBlock: 1, options: {"Subject": ["","Math","Science"]}};
-  }
-  classChange(e) {
-    e.preventDefault();
-    console.log("class changed");
-  }
-  teacherChange(e) {
-    e.preventDefault();
-    console.log("teacher changed");
-  }
-  subjectChange(e) {
-    e.preventDefault();
-    var state = this.state;
-    state.options.Class = ["class 1", "class 2"];
-    state.subject = e.target.value;
-    this.setState(state);
-    console.log("subject changed to "+this.state.subject);
-  }
-  blockChange(e) {
-    e.preventDefault();
-    console.log("block changed");
   }
   render() {
     return (
       <div className={styles.course}>
-        <CourseSelect name="Class" handleChange={this.classChange.bind(this)}
-          options={this.state.options} />
-        <CourseSelect name="Teacher" handleChange={this.teacherChange.bind(this)}
-          options={this.state.options} />
-        <CourseSelect name="Subject" handleChange={this.subjectChange.bind(this)}
-          options={this.state.options} />
-        <CourseSelect name="Block" handleChange={this.blockChange.bind(this)}
-          options={this.state.options} />
+        <CourseSelect name="Class" parentKey={this.props.id} handleChange={this.props.changeHandler}
+          options={this.props.options} />
+        <CourseSelect name="Teacher" parentKey={this.props.id} handleChange={this.props.changeHandler}
+          options={this.props.options} />
+        <CourseSelect name="Subject" parentKey={this.props.id} handleChange={this.props.changeHandler}
+          options={this.props.options} />
+        <CourseSelect name="Block" parentKey={this.props.id} handleChange={this.props.changeHandler}
+          options={this.props.options} />
       </div>
     );
   }
 }
 
 class CourseSelect extends React.Component{
+  handleChange(e) {
+    this.props.handleChange(this.props.parentKey,this.props.name,e.target.value);
+  }
   render() {
     const options = (this.props.options[this.props.name] !== undefined) ?
       this.props.options[this.props.name] : [];
     return (
-      // <label onChange={this.props.onchange}>
-      //TODO write onChange handler for component, pass changes to parent Course
       <label>
         {this.props.name}
-        <select onChange={this.props.handleChange}>
+        <select onChange={this.handleChange.bind(this)}>
           {options.map((option) =>
             <option key={option} value={option}>{option}</option>
           )}
