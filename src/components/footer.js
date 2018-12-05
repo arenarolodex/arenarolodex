908c9c93afc570529d9daf87912ef454178d41ca
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-12-05 12:47:36 -0800

    Add required teachers - Use a checkbox to make a teacher final

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,36 +2,24 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Footer = () => (
-    <div
-        style = {{
-            background: '#da6060',
-            marginBottom: '1.45rem',
-        }}
-    >
-        <div
-            style = {{
-                margin: '0 auto',
-                maxWidth: 960,
-                padding: '1.45rem 1.0875rem',
-            }}
-        >
-            <h1 style={{ margin: 0 }}>
-                <Link
-                    to="/"
-                    style={{
-                        color: 'white',
-                        textDecoration: 'none',
-                    }}
-                >
-                    
-                    Created by <a href="https://github.com/areyoualex">Alex Ruiz</a>
-                    and <a href="https://github.com/WhizardXD">Chris Yuan</a>
-                    <br/>
-                    View source code: <a href="https://github.com/WhizardXD/arenarolodex">https://github.com/WhizardXD/arenarolodex</a>
-
-                </Link>
-            </h1>
-        </div>
+    <div style={{
+      background: '#454141',
+      position: "absolute",
+      width: "100%",
+      bottom: "0",
+      padding: "1rem 2rem"
+    }}>
+      <small style={{ margin: 0 }}>
+        <Link to="/" style={{
+          color: 'white',
+          textDecoration: 'none',
+        }}>
+          Created by <a href="https://github.com/areyoualex">Alex Ruiz</a>
+          &nbsp;and <a href="https://github.com/WhizardXD">Chris Yuan</a>
+          <br/>
+          View source code: <a href="https://github.com/WhizardXD/arenarolodex">https://github.com/WhizardXD/arenarolodex</a>
+        </Link>
+      </small>
     </div>
 )
 
