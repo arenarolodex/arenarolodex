85f6d53d8d91e3b2d15f78b4f9078cc1f5630abd
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-12-05 16:17:32 -0800

    Add notice about live seats

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,13 @@ export default class IndexPage extends React.Component {
   render(){
     return (
       <Layout>
+        <div style={{fontSize:"0.8rem", padding: "0.5rem", backgroundColor: "#ffbd8e",
+          marginBottom: "1rem"}}>
+          <span style={{backgroundColor: "#ff603d", fontWeight:"bold"}}>NOTICE: (12/5/18 4:12 PST)</span><br />
+          <span>Unfortunately, our live seats implementation has been delayed
+          and might not be released today. The information about seats in our
+          website may not be accurate. Thank you for your understanding.</span>
+        </div>
         <Courses displaySchedules={this.schedules.bind(this)} />
         <Schedules schedules={this.state.schedules} />
       </Layout>
