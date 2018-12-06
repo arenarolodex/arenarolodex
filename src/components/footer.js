dd2828096f62b8db64ba24be8febc17510957765
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-12-05 16:02:29 -0800

    Fix content hiding behind footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,18 +7,17 @@ const Footer = () => (
       position: "absolute",
       width: "100%",
       bottom: "0",
-      padding: "1rem 2rem"
+      padding: "1rem 2rem",
     }}>
-      <small style={{ margin: 0 }}>
-        <Link to="/" style={{
-          color: 'white',
-          textDecoration: 'none',
-        }}>
-          Created by <a href="https://github.com/areyoualex">Alex Ruiz</a>
-          &nbsp;and <a href="https://github.com/WhizardXD">Chris Yuan</a>
-          <br/>
-          View source code: <a href="https://github.com/WhizardXD/arenarolodex">https://github.com/WhizardXD/arenarolodex</a>
-        </Link>
+      <small style={{
+        margin: "0",
+        color: 'white',
+        textDecoration: 'none',
+      }}>
+        Created by <a href="https://github.com/areyoualex">Alex Ruiz</a>
+        &nbsp;and <a href="https://github.com/WhizardXD">Chris Yuan</a>
+        <br />
+        View source code: <a href="https://github.com/WhizardXD/arenarolodex">https://github.com/WhizardXD/arenarolodex</a>
       </small>
     </div>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,7 @@ const Layout = ({ children }) => (
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
+        padding: '0px 1.0875rem 8rem',
         paddingTop: 0,
       }}
     >
