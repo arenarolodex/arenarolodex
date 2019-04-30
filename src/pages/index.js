50f641f4b1b282dec7833ef40b917f2f13077e8d
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2019-04-30 16:12:35 -0700

    new announcement

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,10 +28,16 @@ export default class IndexPage extends React.Component {
       <Layout>
         <div style={{fontSize:"0.8rem", padding: "0.5rem", backgroundColor: "#ffbd8e",
           marginBottom: "1rem"}}>
-          <span style={{backgroundColor: "#ff603d", fontWeight:"bold"}}>NOTICE: (12/5/18 7:28 PST)</span><br />
-          <span>Live seats are up and <b>working!</b> Sorry to those who already picked.
-          Arenarolodex will now filter out schedules that have courses with empty
-          seats. Sorry for the delay!</span>
+          <span style={{backgroundColor: "#ff603d", fontWeight:"bold"}}>
+            NOTICE: (04/30/19 4:13 PST)
+          </span>
+          <br />
+          <span>
+            Arenarolodex is almost ready to roll for people to use! We're still working out a 
+            couple of bugs that were birthed while adding new improvements!
+            Apologies for the delay; we anticipate to have everything working around 5.
+            Thanks for your patience!
+          </span>
         </div>
 
           <Courses displaySchedules={this.schedules.bind(this)} loadedCallback={this.loadingCallback} />
