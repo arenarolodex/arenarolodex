dd992f2518171b330f20410fb481e221f3a277f0
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2019-05-03 10:37:14 -0700

    started multiselects
    values are showing up
    styles are all very wonky
    blocks select is not matching up to teachers select

--- src/components/course-ui.jsx
+++ src/components/course-ui.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Select from 'react-select';
+// import Animated from 'react-select/lib/animated';
 
 import styles from './courses.module.css';
 
@@ -9,6 +11,9 @@ class Course extends React.Component {
     this.props.remove(this.props.id);
   }
   render() {
+    // const options = (this.props.options['Teacher'] !== undefined) ?
+    //   this.props.options['Teacher'] : [];
+
     return (
       <div className={styles.course}>
         <button className={styles.deleteBut}
@@ -49,20 +54,143 @@ class CourseSelect extends React.Component {
       );
     const options = (this.props.options && this.props.options[this.props.name] !== undefined) ?
       this.props.options[this.props.name] : [];
-    return (
-      <label>
-        {this.props.name}
-        <div>
-          <select defaultValue={this.props.defValue}
-            onChange={this.handleChange.bind(this)}>
-            {options.map((option) =>
-              <option key={option[1]} value={option[1]}>{option[0]}</option>
-            )}
-          </select>
-          {checkbox}
-        </div>
-      </label>
-    );
+
+    const customStyles = {
+      container: base => ({
+        ...base,
+        height: '22.6666667px',
+        float: 'left',
+      }),
+      control: base => ({
+        ...base,
+        float: 'right',
+        background: 'blue',
+        display: 'flex',
+        height: '22.6666667px',
+        width:'400%',
+        minHeight: 22.66667,
+        fontSize: '16px',
+        '@media (max-width: 800px)': {
+          fontSize: '2vh'
+        },
+      }),
+      valueContainer: base => ({
+        ...base,
+        display: 'flex',
+        position: 'relative',
+        background: 'pink',
+        height: '22.6666667px',
+        minHeight: 22.66667,
+        fontSize: '16px',
+        '@media (max-width: 800px)': {
+          fontSize: '2vh'
+        },
+      }),
+      indicatorsContainer: base => ({
+        ...base,
+        display: 'flex',
+        position: 'relative',
+        background: 'pink',
+        height: '22.6666667px',
+        minHeight: 22.66667,
+        fontSize: '16px',
+        '@media (max-width: 800px)': {
+          fontSize: '2vh'
+        },
+        padding: '0px',
+      }),
+      multiValue: base => ({
+        ...base,
+        background: 'lightYellow',
+        maxWidth: '100px',
+        height: 22.6666667,
+        minHeight: 22.66667,
+        fontSize: '16px',
+        '@media (max-width: 800px)': {
+          fontSize: '2vh'
+        },
+      }),
+      option: base => ({
+        ...base,
+        height: 22.66667,
+        width: '400%',
+        minHeight: 22.66667,
+        fontSize: '16px',
+        '@media (max-width: 800px)': {
+          fontSize: '2vh'
+        },
+      }),
+      menu: base => ({
+        ...base,
+        float: 'right',
+        height: 'auto',
+        width: '400%',
+        minHeight: 22.66667,
+        fontSize: '16px',
+        '@media (max-width: 800px)': {
+          fontSize: '2vh'
+        },
+      }),
+    };
+
+    if (this.props.name === 'Subject' || this.props.name === 'Class') {
+      return (
+        <label>
+          {this.props.name}
+          <div>
+            <select defaultValue={this.props.defValue}
+              onChange={this.handleChange.bind(this)}>
+              {options.map((option) =>
+                <option key={option[1]} value={option[1]}>{option[0]}</option>
+              )}
+            </select>
+            {checkbox}
+          </div>
+        </label>
+      );
+    } else if (this.props.name === 'Teacher') {
+      return (
+        <label>
+          {this.props.name}
+          <div>
+            <Select
+              isMulti
+              placeholder="Select a class"
+              styles={customStyles}
+              options={
+                options.map((option) => {
+                  return {
+                    value: option[1],
+                    label: option[0]
+                  };
+                })
+              }
+            />
+          </div>
+        </label>
+      );
+    } else if (this.props.name === 'Block') {
+      return (
+        <label>
+          {this.props.name}
+          <div>
+            <Select
+              isMulti
+              placeholder="Select a teacher"
+              styles={customStyles}
+              options={
+                options.map((option) => {
+                  return {
+                    value: option[1],
+                    label: option[0]
+                  };
+                })
+              }
+            />
+          </div>
+        </label>
+      );
+    }
   }
 }
 
