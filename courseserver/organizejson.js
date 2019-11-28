89db38ecf1ba8ce4f0a820f442738b7c83b93c9a
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2019-11-28 02:30:39 -0800

    added spring 2020 announcer

--- courseserver/organizejson.js
+++ courseserver/organizejson.js
@@ -1,44 +1,59 @@
-const fs = require('fs');
+const fs = require("fs");
 
-fs.readFile('announcer.json', (err, data) => {
+fs.readFile("announcer.json", (err, data) => {
   if (err) throw err;
   //Get data from original announcer
   const ogannouncer = JSON.parse(data);
 
   //This object will be the object we write JSON data from
   var newannouncer = {
-    "Math":{},
-    "Science":{},
-    "English":{},
-    "Social Studies":{},
-    "VPA":{},
-    "World Language":{},
-    "PE":{},
-    "Other":{}
+    "Math": {},
+    "Science": {},
+    "English": {},
+    "Social Studies": {},
+    "VPA": {},
+    "World Languages": {},
+    "Physical Education": {},
+    "Other": {},
+    "Learning Services": {}
   };
 
   //Read each course, write them to newannouncer
-  ogannouncer.forEach((course) => {
+  ogannouncer.forEach(course => {
     //Get course information (just convenience for getting keys)
-    const deptKey = ["Math","Science","English","Social Studies","VPA",
-      "World Language", "PE", "Other"];
-    const deptIndex = deptKey[parseInt(course["Department Number"])-1]
+    // const deptKey = [
+    //   "Math",
+    //   "Science",
+    //   "English",
+    //   "Social Studies",
+    //   "VPA",
+    //   "World Language",
+    //   "PE",
+    //   "Other"
+    // ];
+    // const deptIndex = deptKey[parseInt(course["Department Number"]) - 1];
+    const deptIndex = course["Department Name"]
     const name = course["Course Name"];
     const teacher = course["Teacher"];
     // console.log(course);
 
     //Make objects that don't exist yet
-    if (!newannouncer[deptIndex][name])
-      newannouncer[deptIndex][name] = {};
+    if (!newannouncer[deptIndex][name]) newannouncer[deptIndex][name] = {};
     if (!newannouncer[deptIndex][name][teacher])
       newannouncer[deptIndex][name][teacher] = [];
 
-    newannouncer[deptIndex][name][teacher].push([course["Block"], course["Room"], "34"]);
+    newannouncer[deptIndex][name][teacher].push([
+      course["Block"],
+      course["Room"],
+      "34"
+    ]);
   });
 
   //Write newannouncer.json
-  const announcerData = new Uint8Array(Buffer.from(JSON.stringify(newannouncer)));
-  fs.writeFile('newannouncer.json', announcerData, (err) => {
+  const announcerData = new Uint8Array(
+    Buffer.from(JSON.stringify(newannouncer))
+  );
+  fs.writeFile("newannouncer.json", announcerData, err => {
     if (err) throw err;
     console.log("newannouncer.json has been written.");
   });
